c5a87bd98945ca680784196fc142ae2eb8ac63b2
Scott Vojik <scott.vojik@gmail.com>
2019-02-24 21:35:46 -0800

    exercise complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 
@@ -8,14 +9,40 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
-  res.send('Its Alive!');
+server.post('/api/login', (req, res) => {
+  const creds = req.body;
+
+  db('users').where({ username: creds.username }).
+  first()
+  .then(user => {
+    if (user && bcrypt.compareSync(creds.password, user.password)) {
+      res.status(200).json({ message: 'welcome' })
+    } else {
+      res.status(401).json({ message: 'error' });
+    }
+  })
+  .catch(err => res.json)
 });
+
+
+server.post('/api/register', (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 14);
+  creds.password = hash;
+
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => json(err));
+});
+
 
 // protect this route, only authenticated users should see it
 server.get('/api/users', (req, res) => {
   db('users')
-    .select('id', 'username')
+    .select('id', 'username', 'password')
     .then(users => {
       res.json(users);
     })
